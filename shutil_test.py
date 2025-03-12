86474acbe1d0e8aae67fc4ab731962a3692e1d49
il-gwang <parkik9472@gmail.com>
2025-03-12 23:13:23 +0900

    검증 완료

--- shutil_test.py
+++ shutil_test.py
@@ -1,15 +1,14 @@
 import os
 import shutil
 
-folder_path = "C:/Users/302/Documents/새 폴더/data/dogs-vs-cats/train" # C:\Users\302\Documents\새 폴더\data\dogs-vs-cats\train
-
+folder_path = "C:/python_Ex/dogs-vs-cats/train" #실제 사진이 있는 폴더 경로
 #print('os 모듈 = ', os.getcwd())
 
-def create_folder(folder_name):
-    os.makedirs(os.path.join(folder_path,folder_name),exist_ok=True)
+def create_folder(folder_name): #폴더 만들기 함수
+    os.makedirs(os.path.join(folder_path,folder_name),exist_ok=True)#폴더를 지정한 이름으로 생성한다.
 
 
-def sort_files():
+def sort_files():#파일을 cat, dog로 시작하는 파일을 폴더에 
     for file in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file)
         if os.path.isfile(file_path):  # 파일인지 확인
